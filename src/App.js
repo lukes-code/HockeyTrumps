b32d73059128975f76fd0a800a6d653d9852ff28
Johannes Pineker <johannes@pineker.dev>
2022-10-01 01:52:49 +0200

    Add generation of card hands

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
 import CardContainer from './components/CardContainer';
+import data from './players.json';
 
 function App() {
+  const cpuCards = [...data.players];
+  cpuCards.sort(function (a, b) {
+    return 0.5 - Math.random();
+  });
+
+  const playerCards = cpuCards.splice(0, Math.floor(cpuCards.length / 2));
+  while (playerCards.length < cpuCards.length) {
+    cpuCards.pop();
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Hockey Trumps</h1>
+        <span>
+          First Card Player: {playerCards[0].name} | Cardpool Size:{' '}
+          {playerCards.length}
+        </span>
+        <span>
+          First Card CPU: {cpuCards[0].name} | Cardpool Size: {cpuCards.length}
+        </span>
         <CardContainer />
       </header>
     </div>
