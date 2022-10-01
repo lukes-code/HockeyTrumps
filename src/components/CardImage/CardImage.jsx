5f744adff8fd1be5d3a68c5a65f9f63701e44340
Luke Sterry <lukesterryy@gmail.com>
2022-10-01 14:20:57 +0100

    Initial styling for cards #6

--- src/components/CardImage/CardImage.jsx
+++ src/components/CardImage/CardImage.jsx
@@ -1,7 +1,9 @@
 export default function CardImage( {img, name, team} ) {
   return (
-    <div className="card-img">
-      <img alt={`Player portrait of ${name}`} src={img} />
+    <div className="card-top">
+      <div className="card-img">
+        <img alt={`Player portrait of ${name}`} src={img} />
+      </div>
       <span className="player-name">{name}</span>
       <span className="player-team">{team}</span>
     </div>
